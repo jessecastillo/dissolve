51fb4345d2da9b189a34d23d5447591f6f50f000
Jesse Castillo <6.24jcastillorolltide@gmail.com>
2023-08-05 01:55:45 -0500

    scrollEffect

--- app/Video.js
+++ app/Video.js
@@ -8,8 +8,9 @@ const VideoSection = () => {
         <source src="/videos/Aquatic_Clouds_geo_HD.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div className="absolute left-1/2 bottom-1/4 transform -translate-x-1/2 -translate-y-1/2">
-        {/* Adjust top and left to move the arrow */}
+      <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+        {/* Add the text above the arrow */}
+        <p className="text-xl text-white font-semibold mb-4">For those who Seek...</p>
         <Link href="#product-section">
           {/* Replace the existing arrow with a Font Awesome arrow */}
           <i className="text-4xl fas fa-chevron-down"></i>
